084b6eaeeaf0af01b4c694d63a54ccca91cfbe21
Pavel Rusinov <prusinov@tradingview.com>
2023-01-26 17:53:09 +0400

    DF-1575: requested changes

--- update_example/update_example.md
+++ update_example/update_example.md
@@ -1,12 +1,12 @@
 # Update script example
 
-As an example history updating we use script that gets data from https://api.santiment.net/
+As an example history updating we use script that gets data from https://api.santiment.net/ (Links: [Project main page](https://santiment.net/), [Santiment API description](https://academy.santiment.net/sanapi/), [Santiment metrics description](https://academy.santiment.net/metrics/#financial))
 Folder that contains update_santiment.py and update.sh should be placed near your fork local copy (e. g. ~/github/fork_local_copy, ~/github/folder_with_update_scripts).
 
 To make update you should run update.sh from folder that cointains it: `cd ~/github/folder_with_update_scripts && ./update.sh`
 
-update.sh runs update_santiment.py which gets data from santiment api and writes it to csv files with history, update.sh then commits changes and pushes them, after that changes will go to tradingview.
+update.sh runs update_santiment.py which gets data from Santiment API and writes it to CSV files with history, update.sh then commits changes and pushes them, after that changes will go to TradingView.
 
 In more detail about what the script update_santiment.py:
-This script requests several metric values for 2 crypto assets: ethereum and bitcoin. For each crypto asset metric, a file \<CRYPTO_ASSET_FILE_PREFIX>_<METRIC_OR_FIXED_POSTFIX>.csv is updated (e. g. for bitcoin sentiment_negative_total metric it updates data/seed_crypto_santiment/BTC_SENTIMENT_NEGATIVE_TOTAL.csv, for ethereum unique_social_volume_total_1h data/seed_crypto_santiment/ETH_UNIQUE_SOCIAL_VOLUME_TOTAL.csv, its prefix is not metric name in upper case because we added fix for this metric).
-For each crypto asset metric we make http request to santiment graphql api (we request all data up to today inclusive) and save updated values to corresponding file. If there is a value for a date in the file, but it is not in the response from the api, the value for this date remains unchanged in the file.
+This script requests several metric values for 2 crypto assets: Ethereum and Bitcoin. For each crypto asset metric, a file \<CRYPTO_ASSET_FILE_PREFIX>_<METRIC_SUFFIX>.csv is updated (e. g. for Bitcoin sentiment_negative_total metric it updates data/seed_crypto_santiment/BTC_SENTIMENT_NEGATIVE_TOTAL.csv, for Ethereum unique_social_volume_total_1h data/seed_crypto_santiment/ETH_UNIQUE_SOCIAL_VOLUME_TOTAL_1H.csv).
+For each crypto asset metric we make HTTP request to Santiment GraphQL API (we request all data up to today inclusive) and save updated values to corresponding file. If there is a value for a date in the file, but it is not in the response from the API, the value for this date remains unchanged in the file.
--- update_example/update_santiment.py
+++ update_example/update_santiment.py
@@ -1,5 +1,6 @@
-"""Script to get metrics history for bitcoin and ethereum
-from santiment api and save updates to csv files in required format"""
+"""This script downloads data from Santiment API, converts it to
+Pine Seeds historical data format and saves it as CSV files."""
+import argparse
 import csv
 import os
 from datetime import datetime
@@ -9,15 +10,15 @@ import requests
 
 
 @dataclasses.dataclass
-class SlugInfo:
-    """crypto coin name in santiment api and data
-    filename prefix when saving values to csv file"""
+class CoinInfo:
+    """crypto coin name in Santiment API and data
+    filename prefix when saving values to CSV file"""
 
     slug: str
     data_filename_prefix: str
 
 
-# metrics for crypto coins to request from santiment api
+# metrics for crypto coins to request from Santiment API
 METRICS = [
     "sentiment_negative_total",
     "github_activity",
@@ -30,27 +31,22 @@ METRICS = [
 ]
 
 # crypto coins for which you need to request metrics
-# and corresponding csv files prefixes
-SLUG_INFOS = [
-    SlugInfo(
+# object contains slug - Santiment API field value to request this coin
+# and data_filename_prefix - prefix for CSV file for this coin
+COIN_INFOS = [
+    CoinInfo(
         slug="bitcoin",
         data_filename_prefix="btc",
     ),
-    SlugInfo(
+    CoinInfo(
         slug="ethereum",
         data_filename_prefix="eth",
     ),
 ]
 
-# metric names are used in csv files as postfixes
-# this mapping is used to replace metric with another value
-DATA_FILENAME_POSTFIX_FIXES = {
-    "unique_social_volume_total_1h": "unique_social_volume_total"
-}
-
-REQ_URL = "https://api.santiment.net/graphql"
-REQ_HEADERS = {"Content-Type": "application/graphql"}
-REQ_BODY_TEMPLATE = """
+REQUEST_URL = "https://api.santiment.net/graphql"
+REQUEST_HEADERS = {"Content-Type": "application/graphql"}
+REQUEST_BODY_TEMPLATE = """
 { getMetric(metric: "%s")
   { 
     timeseriesData( 
@@ -67,35 +63,33 @@ REQ_BODY_TEMPLATE = """
 }
 """
 
-DATA_FOLDER = "../seed_crypto_santiment/data/seed_crypto_santiment/"
-
 
 def save_new_data(data_json, filename):
-    """save updated values to csv"""
-    orig = {}
+    """save updated values to CSV"""
+    data_dict = {}
 
     if os.path.exists(filename):
         with open(filename, "r", encoding="utf-8") as file:
             for line in csv.reader(file):
-                orig[line[0]] = line[1]
+                data_dict[line[0]] = line[1]
 
     for data_point in data_json:
         date = data_point["datetime"]
         val = data_point["value"]
-        orig[date] = val
+        data_dict[date] = val
 
     with open(filename, "w", encoding="utf-8") as file:
         writer = csv.writer(file)
-        for date in sorted(orig.keys()):
-            val = orig[date]
+        for date in sorted(data_dict.keys()):
+            val = data_dict[date]
             writer.writerow([date, val, val, val, val, 0])
 
 
-def process_data_file(slug_info, metric, to_date):
-    """request data from santiment api and save updated values to file"""
-    req_body = REQ_BODY_TEMPLATE % (metric, slug_info.slug, to_date)
+def process_data_file(data_folder, coin_info, metric, to_date):
+    """request data from Santiment API and save updated values to file"""
+    req_body = REQUEST_BODY_TEMPLATE % (metric, coin_info.slug, to_date)
 
-    response = requests.post(REQ_URL, headers=REQ_HEADERS, data=req_body)
+    response = requests.post(REQUEST_URL, headers=REQUEST_HEADERS, data=req_body)
 
     data_json = response.json()["data"]["getMetric"]["timeseriesData"]
 
@@ -103,27 +97,34 @@ def process_data_file(slug_info, metric, to_date):
         end = data_point["datetime"].index("T") + 1
         data_point["datetime"] = data_point["datetime"][:end].replace("-", "")
 
-    # use metric as postfix, if there is a fix for this metric, use it
-    filename_postfix = DATA_FILENAME_POSTFIX_FIXES.get(metric, metric)
-    filename = (
-        slug_info.data_filename_prefix + "_" + filename_postfix
-    ).upper() + ".csv"
-    out_path = DATA_FOLDER + filename
+    # use metric as suffix
+    filename = (coin_info.data_filename_prefix + "_" + metric).upper() + ".csv"
+    out_path = os.path.join(data_folder, filename)
 
     save_new_data(data_json, out_path)
-    print("updated", slug_info.slug, metric)
+    print("updated", coin_info.slug, metric)
+
+
+def parse_args():
+    """parse args"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--data-folder", default=".", type=str)
+
+    return parser.parse_args()
 
 
 def main():
     """main func"""
-    if not os.path.exists(DATA_FOLDER):
-        os.makedirs(DATA_FOLDER)
+    args = parse_args()
+    if not os.path.exists(args.data_folder):
+        os.makedirs(args.data_folder)
 
     to_date = datetime.today().strftime("%Y-%m-%d") + "T23:59:59Z"
 
-    for slug_info in SLUG_INFOS:
+    for slug_info in COIN_INFOS:
         for metric in METRICS:
-            process_data_file(slug_info, metric, to_date)
+            process_data_file(args.data_folder, slug_info, metric, to_date)
 
 
 if __name__ == "__main__":
